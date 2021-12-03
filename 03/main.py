ee90caa3ad71cdf694beeb5eac1ea7a32953031a
Rokaeus <gustav@rokaeus.se>
2021-12-03 17:06:51 +0100

    1 star

--- 03/main.py
+++ 03/main.py
@@ -5,7 +5,7 @@ def convert_input_to_list(path):
         list = [str(entry.rstrip().replace(" ","")) for entry in input]
         return list
 
-def solve_gamma_rate(input):
+def solve_common_bit_gamma(input):
     binaryresults = []
     for i in range(0,(len(input[0]))):
         zeros = 0
@@ -23,4 +23,40 @@ def solve_gamma_rate(input):
 
     return binaryresults
 
-print(solve_gamma_rate(convert_input_to_list("inputtest.txt")))
+def solve_common_bit_epsilon(input):
+    binaryresults = []
+    for i in range(0,(len(input[0]))):
+        zeros = 0
+        ones = 0
+        for row in input:
+            if row[i] == "0":
+                zeros += 1
+            elif row[i] == "1":
+                ones += 1
+        
+        if zeros > ones:
+            binaryresults.append(1)
+        elif ones > zeros:
+            binaryresults.append(0)
+
+    return binaryresults
+
+def binaryToDecimal(n):
+    return int(n,2)
+
+def solve_gamma(list):
+    gamma_string = ""
+    for x in list:
+        gamma_string += str(x)
+    return (binaryToDecimal(gamma_string))
+
+def solve_epsilon(list):
+    epsilon_string = ""
+    for x in list:
+        epsilon_string += str(x)
+    return (binaryToDecimal(epsilon_string))
+
+gamma = solve_gamma(solve_common_bit_gamma(convert_input_to_list("input.txt")))
+epsilon = solve_epsilon(solve_common_bit_epsilon(convert_input_to_list("input.txt")))
+consumtion = gamma * epsilon
+print(consumtion)
